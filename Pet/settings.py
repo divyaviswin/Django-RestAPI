4eb808bbf165241fda9b12a6fde6f2e4be9558fc
Divya <divyaviswin@gmail.com>
2017-12-21 16:11:07 +0400

    Static files

--- Pet/settings.py
+++ Pet/settings.py
@@ -120,8 +120,10 @@ LOGIN_REDIRECT_URL='/'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+STATIC_ROOT=os.path.join(BASE_DIR, "staticcdn","static_root")
+MEDIA_URL = '/media/'
+MEDIA_ROOT=os.path.join(BASE_DIR, "staticcdn","media_root")
--- Pet/urls.py
+++ Pet/urls.py
@@ -25,4 +25,7 @@ urlpatterns = [
     url(r'^api-auth/', include('rest_framework.urls')),
 
 
-]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
+if settings.DEBUG:
+    urlpatterns=urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
